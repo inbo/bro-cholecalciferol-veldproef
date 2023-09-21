6f25e0ed2924c7c4dd7cdca38f6a696246aa8b1d
EmmaCartuyvels1 <emma_cartuyvels@hotmail.com>
2023-09-21 15:02:48 +0200

    Add pre-bait analyses

--- src/AnalyseOpname.R
+++ src/AnalyseOpname.R
@@ -1,6 +1,7 @@
 # Analyse opname
 
 library(readxl)
+library(lubridate)
 library(tidyverse)
 
 # Pre-census
@@ -15,11 +16,24 @@ ggplot(precensus, aes(x = Datum, y = value, color = name)) +
   ylim(800, 1050) +
   ylab("Gewicht lokaasdoos + graan (g)") + 
   theme(legend.position = "none")  
-       
+      
+# Pre-bait
+prebait <- read_xlsx("input/Opname graan, pre-bait en gif.xlsx",
+                       sheet = "Pre-bait") %>% 
+  pivot_longer(2:3) %>% 
+  rename(Punt = `...1`, Datum = name, Opname = value) %>% 
+  mutate(Datum = as.Date(as.numeric(Datum), origin = '1899-12-30'))
+
+ggplot(prebait, aes(x = Datum, y = Opname, color = as.factor(Punt))) +
+  geom_point() +
+  geom_line() +
+  #ylim(800, 1000) +
+  ylab("Gewicht lokaasdoos + Harmonix pre-bait (g)") + 
+  theme(legend.position = "none")
 
 # Bestrijding
 bestrijding <- read_xlsx("input/Opname graan, pre-bait en gif.xlsx",
-                       sheet = "Blad2") %>% 
+                       sheet = "Vergif") %>% 
   pivot_longer(2:30) %>% 
   mutate(name = as.factor(name))
 
